8f7971f0328990ea47cd00830cdea6633d2c5f6b
andrea.grasserbauer@gmail.com <andrea.grasserbauer@gmail.com@5a707c3c-09a1-c401-cd80-3f316e3f242b>
2015-02-12 21:46:19 +0000

    Version with agents

--- trunk/routing/routing/main.cpp
+++ trunk/routing/routing/main.cpp
@@ -3,29 +3,83 @@
 #include "planner.h"
 
 #define MAZEHEIGHT 25
-#define MAZEWIDTH  25
+#define MAZEWIDTH  50
+#define DIRECTIONS  4
+#define MAZEDENSITY 0.40
+#define WALLSTOREMOVE 40
 #define GOALS       4
 #define AGENTS      2
 
 int main(){
 
 	int startx, starty, goalx, goaly, tmpstartx, tmpstarty, oldgoaly, oldgoalx, primestartx, primestarty;
+	int mapx, mapy, newx, newy;
+	int wallstoremove = WALLSTOREMOVE;
 
-	list<Planner> agent;
+	int agent_startx, agent_starty;
+
+	int *dyn_goalx = 0;
+
+	dyn_goalx = new int[GOALS];
+
+	int *dyn_goaly = 0;
+	dyn_goaly = new int[GOALS];
+
+	int *dyn_agentx = 0;
+	dyn_agentx= new int[AGENTS];
+
+	int *dyn_agenty = 0;
+	dyn_agenty = new int[AGENTS];
+
+
+
+	list<Planner> goals;
 	std::list<Planner>::iterator it;
 	list<Map::Cell *> path;	
+	list<Map::Cell *> lastpath;
+	list<Map::Cell *> agentpath;
 
 	Map *map = new Map(MAZEWIDTH, MAZEHEIGHT);
 	//(*map).
 	//more readable 
 	//map->operator()
-	for (int i = 0; i < 20; ++i) {
-		for (int j = 0; j < 20; ++j) {
+	for (int i = 0; i < MAZEWIDTH; ++i) {
+		for (int j = 0; j < MAZEHEIGHT; ++j) {
 			(*map)(i, j)->cost = 1;
 		}
 	}
 
-	(*map)(1, 5)->cost = Map::Cell::COST_UNWALKABLE;
+	//NEEDED MORE RANDOMIZED OBSTACLES
+
+	while (wallstoremove)
+	{
+		newx = rand() % MAZEWIDTH;
+		newy = rand() % MAZEHEIGHT;
+		(*map)(newx, newy)->cost = Map::Cell::COST_UNWALKABLE;
+		--wallstoremove;
+	}
+
+	for (int x = 0; x < MAZEWIDTH + 2; ++x)
+		std::cout << "X";
+	std::cout << std::endl;
+	for (int y = 0; y < MAZEHEIGHT; ++y) {
+		std::cout << "X";
+		for (int x = 0; x < MAZEWIDTH; ++x){
+			if ((*map)(x, y)->cost == Map::Cell::COST_UNWALKABLE)
+				std::cout << "X";
+			else
+				std::cout << " ";
+
+		}
+		std::cout << "X" << std::endl;
+
+	}
+	for (int x = 0; x < MAZEWIDTH + 2; ++x)
+		std::cout << "X";
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	
 
 	std::srand(std::time(0));
 
@@ -48,17 +102,25 @@ int main(){
 			tmpstarty = starty;
 			primestartx = startx;
 			primestarty = starty;
+			dyn_goalx[i] = startx;
+			dyn_goaly[i] = starty;
 		}
 		else {
 			tmpstartx = oldgoalx;
 			tmpstarty = oldgoaly;
 		}
 
+
 		Planner p(map, (*map) (tmpstartx, tmpstarty), (*map) (goalx, goaly));
 		oldgoalx = goalx;
-		oldgoaly = goaly;
-		agent.push_back(p);	
+		oldgoaly = goaly;	
+
+		
+		dyn_goalx[i+1] = goalx;
+		dyn_goaly[i+1] = goaly;
+
 		p.replan();
+		goals.push_back(p);
 		path = p.path();
 		std::cout <<  "Path" << i << std::endl;
 		std::cout << "Start x: " << tmpstartx << std::endl;
@@ -75,6 +137,8 @@ int main(){
 	}
 
 	Planner p(map, (*map) (oldgoalx, oldgoaly), (*map) (primestartx, primestarty));
+	p.replan();
+	lastpath = p.path();
 
 	std::cout << "Last Path" << std::endl;
 	std::cout << "Start x: " << oldgoalx << std::endl;
@@ -82,12 +146,58 @@ int main(){
 	std::cout << "Goal x: " << primestartx << std::endl;
 	std::cout << "Goal y: " << primestarty << std::endl;
 
-	while (!path.empty()) {
+	while (!lastpath.empty()) {
 		int r, c;
-		r = path.front()->x();
-		c = path.front()->y();
-		path.pop_front();
+		r = lastpath.front()->x();
+		c = lastpath.front()->y();
+		lastpath.pop_front();
 		std::cout << r << ":" << c << std::endl;
+	}
+
+	for (int i = 0; i < AGENTS; i++) {
+		goaly = (rand() % ((MAZEHEIGHT + 1) / 2)) * 2;
+		goalx = (rand() % ((MAZEWIDTH + 1) / 2)) * 2;
+		while (1)
+		{
+			starty = (rand() % ((MAZEHEIGHT + 1) / 2)) * 2;
+			startx = (rand() % ((MAZEWIDTH + 1) / 2)) * 2;
+			if (startx != goalx || starty != goaly)
+				break;
+		}
+
+		agent_startx = startx;
+		agent_starty = starty;
+
+		dyn_agentx[i] = startx;
+		dyn_agenty[i] = starty;
+
+		std::cout << "Agent" << i << std::endl;
+		std::cout << "Start x: " << dyn_agentx[i] << std::endl;
+		std::cout << "Start y: " << dyn_agenty[i] << std::endl;
+		std::cout << "First Goal x: " << dyn_goalx[i] << std::endl;
+		std::cout << "First Goal y: " << dyn_goaly[i] << std::endl;
+
+		//ACTUALLY UNNECESSARY AS WE ALREADY HAVE STORED THE SHORTEST PATH IN GOALS LIST
+		//SO WE NEED ONLY THE PATH TO FIRST GOAL PER AGENT
+
+		for (int j = 0; j < GOALS; j++) {
+			if (j == 0)
+				Planner p(map, (*map) (startx, starty), (*map) (dyn_goalx[j], dyn_goaly[j]));
+			else
+				Planner p(map, (*map) (dyn_goalx[j - 1], dyn_goaly[j-1]), (*map) (dyn_goalx[j], dyn_goaly[j]));
+			p.replan();
+			agentpath = p.path();
+		}
+		
+		while (!agentpath.empty()) {
+			int r, c;
+			r = agentpath.front()->x();
+			c = agentpath.front()->y();
+			agentpath.pop_front();
+			std::cout << r << ":" << c << std::endl;
+		}
+
+
 	}
 
 	
