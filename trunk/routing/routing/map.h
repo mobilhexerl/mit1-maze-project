7f4573c48044f12c5c96b06f076e3207b9478026
andrea.grasserbauer@gmail.com <andrea.grasserbauer@gmail.com@5a707c3c-09a1-c401-cd80-3f316e3f242b>
2015-02-23 00:12:23 +0000

    hash constant set to double

--- trunk/routing/routing/map.h
+++ trunk/routing/routing/map.h
@@ -36,7 +36,7 @@ namespace DStarLite
 							/**
 							 * @var  int  hash "constant" (may need to change if width exceeds this value)
 							 */
-							static const int C;
+							static const double C;
 
 							/**
 							 * Hashes cell based on coordinates.
@@ -44,7 +44,9 @@ namespace DStarLite
 							 * @param   Cell*
 							 * @return  size_t
 							 */
-							size_t operator()(Cell* c) const;
+							//size_t operator()(Cell* c) const;
+							double_t operator()(Cell* c) const;
+							
 					};
 
 					/**
