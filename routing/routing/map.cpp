cacc2187b5a4a19fc8f79b1c3109b1ffe63fa68c
andrea.grasserbauer <andrea.grasserbauer@gmail.com>
2015-02-23 15:13:15 +0000

    reset hash const, bug fix blocking

--- routing/routing/map.cpp
+++ routing/routing/map.cpp
@@ -24,9 +24,7 @@ const double Map::Cell::COST_UNWALKABLE = DBL_MAX;
 /**
  * @var  int  hash "constant" (may need to change if map width exceeds this value)
  */
-//const int Map::Cell::Hash::C = 1000000;
-
-const double Map::Cell::Hash::C = 1000000000000000;
+const int Map::Cell::Hash::C = 1000000;
 
 /**
  * Constructor.
@@ -260,7 +258,7 @@ unsigned int Map::Cell::y()
  * @param   Cell*
  * @return  size_t
  */
-double_t Map::Cell::Hash::operator()(Cell* c) const
+size_t Map::Cell::Hash::operator()(Cell* c) const
 {
 	return Cell::Hash::C * c->y() + c->x();
 }
